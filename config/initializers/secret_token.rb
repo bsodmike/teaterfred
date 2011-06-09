01c853e42c2b8ed1c868a482986067a01b0b82b4
Daniel <znowm4n@gmail.com>
2011-06-09 13:32:10 +0200

    update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teaterfred::Application.config.secret_token = '8c66af706417d7a855692593453af1160ec358a7a7d6fd9ab53c988e1e001cc59331c0965bc4a1b5b4ed2e739c8b8222555c694e77844ce6ad2417a084229526'
