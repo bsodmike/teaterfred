e379a790306a405b9f9ef75288f95a7b7ee3b61b
Daniel <znowm4n@gmail.com>
2011-07-07 23:53:15 +0200

    updated application.haml, added flash styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,15 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to root_url, :notice => "Du er nu blevet logget ind"
     else
-      flash.now.alert = "Invalid email or password"
-      render "new"
+      render "new", :error => 'Forkert email eller kodeord'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Logged out!"
+    redirect_to root_url, :alert => "Du er nu blevet logget ud"
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      session[:user_id] = user.id 
-      redirect_to root_url, :notice => "Signed up"
+      session[:user_id] = @user.id
+      redirect_to root_url, :notice => "Du er nu oprettet som bruger"
     else
-      render :new
+      render :new, :error => 'Brugeren kunne ikke oprettes!'
     end
   end
 
