b176e491384762f8e7e0033c39fd4fd490dd0800
Daniel <znowm4n@gmail.com>
2011-07-13 16:29:58 +0200

    updated news and boards controller to not use inherited ressources

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,4 @@
 class EventsController < InheritedResources::Base
-
   before_filter :signed_in_user, :only => [:new, :create, :edit, :update, :destroy]
 
   def index
--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -1,2 +1,44 @@
-class NewsItemsController < InheritedResources::Base
+class NewsItemsController < ApplicationController
+  
+  def index
+    @news_items = NewsItem.all
+  end
+  
+  def show
+    @news_item = NewsItem.find(params[:id])
+  end
+  
+  def new
+    @news_item = NewsItem.new
+  end
+
+  def edit
+    @news_item = NewsItem.find(params[:id])
+  end
+
+  def create
+    @news_item = NewsItem.new(params[:news_item])
+
+    if @news_item.save
+      redirect_to(@news_item, :notice => 'Nyheden blev oprettet')
+    else
+      render :new, :error => 'Nyheden blev ikke oprettet'
+    end
+  end
+
+  def update
+    @news_item = NewsItem.find(params[:id])
+    if @news_item.update_attributes(params[:news_item])
+      redirect_to(@news_item, :notice => 'Nyheden blev odpateret')
+    else
+      render :edit, :error => 'Nyheden blev ikke opdateret'
+    end
+  end
+
+  def destroy
+    @news_item = NewsItem.find(params[:id])
+    @news_item.destroy
+    redirect_to(news_items_url)
+  end
+  
 end
