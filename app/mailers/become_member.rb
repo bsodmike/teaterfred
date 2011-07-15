83f0a0384aee8efefd7423d29fc95ab6a56c0b80
Daniel <znowm4n@gmail.com>
2011-07-15 14:07:02 +0200

    updated

--- app/mailers/become_member.rb
+++ app/mailers/become_member.rb
@@ -1,6 +1,9 @@
 class BecomeMember < ActionMailer::Base
   default from: "info@teaterfred.dk"
   
-  
+  def test
+    @url = '/become_member'
+    mail(:to => 'info@teaterfred.dk', :subject => "Ansøgning om medlemsskab")
+  end
   
 end
