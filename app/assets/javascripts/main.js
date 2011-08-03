2efa30b4e8e7f5503c65d9dd51b2bfbd83dfbd42
Daniel G <znowm4n@gmail.com>
2011-08-03 15:20:09 +0200

    added plupload-rails3 plugin, not working yet

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -21,6 +21,38 @@ $(function() {
       monthNames:['Januar', 'Februar', 'Marts', 'April', 'Maj', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'December']
   });
   
+  $(".lightbox").click(function(e) {
+    e.preventDefault();
+    var image_href = $(this).attr("href");
+    
+    if ($("#lightbox").length > 0) {
+      //insert img tag with clicked link's href as src value
+      $('#content').html('<img src="' + image_href + '" />');
+      
+      // show lightbox window - you can use a transition here if you want
+      $('#lightbox').show();
+    }
+    else { //#lightbox does not exist
+      
+      //create HTML markup for lightbox window
+      var lightbox =
+      '<div id="lightbox">' + 
+        '<p>Klik for at lukke</p>' +
+        '<div id="content">' + //insert clicked link's href into img src
+          '<img src="' + image_href +'" />' +
+        '</div>' +
+      '</div>';
+    
+      //insert lightbox HTML into page
+      $('body').append(lightbox); 
+    }
+    
+   //Click anywhere on the page to get rid of lightbox window 
+   $("#lightbox").live('click', function() { //must use live, as the lightbox element is inserted into the DOM
+     $("#lightbox").hide();
+   });
+  });
+  
   
   function eventRenderer(event, element, view) {
       if (view.start.getMonth() == event.start.getMonth()) {
