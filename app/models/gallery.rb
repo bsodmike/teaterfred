f924fd3ee2694ea89dac28e2dc96eea2334c3d42
Daniel G <znowm4n@gmail.com>
2011-08-05 14:17:50 +0200

    initial update

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 
 class Gallery < ActiveRecord::Base
-  validates :name, :presence => true, :uniqueness => { :case_sensitive => false }
+  validates :title, :presence => true, :uniqueness => { :case_sensitive => false }
   validates :place, :presence => true
   has_many :images#, :source => 'gallery_id'
 
