256e739d72bfc5571419543590a183c86586941b
Daniel G <znowm4n@gmail.com>
2011-08-05 14:43:10 +0200

    initial update

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -3,6 +3,6 @@
 class Gallery < ActiveRecord::Base
   validates :title, :presence => true, :uniqueness => { :case_sensitive => false }
   validates :place, :presence => true
-  has_many :images#, :source => 'gallery_id'
+  has_many :images
 
 end
