46fbf8575b1128851f6b2f49195a091d3d9dce82
Daniel <znowm4n@gmail.com>
2011-08-06 02:33:06 +0200

    Updated image migration

--- db/migrate/20110806002925_create_images.rb
+++ db/migrate/20110806002925_create_images.rb
@@ -1,6 +1,6 @@
 class CreateImages < ActiveRecord::Migration
   def self.up
-    create_tables :images do |t|
+    create_table :images do |t|
       t.string :title
       t.string :image
       t.integer :gallery_id
